6116eadd975faa485357a617b944cfa3d16bc19a
Noel De Martin <noel@noeldemartin.com>
2026-04-22 11:04:52 +0200

    Configure deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vsix-download/' : '/',
   plugins: [vue(), tailwindcss()],
   fmt: { semi: true, singleQuote: true },
   lint: { options: { typeAware: true, typeCheck: true } },
